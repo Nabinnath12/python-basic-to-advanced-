e7e22034b87c7e6993eb47860a635f1a7fb7b7a3
nabin nath <xettrikto@icloud.com>
2025-09-30 20:48:28 +0545

    python basic to advanced

---
+++ first.py
@@ -0,0 +1,19 @@
+
+a = float(input("please enter the first numbet::  "))
+b=float(input("please enter the second number::  "))
+
+
+addition= a+b
+subtraction= a-b
+mul=a*b
+if a !=0: 
+
+    divide = a/b
+
+else:
+    divide = "can not divide by zero"
+
+# print(f"the addition of the two number is :::",addition)
+# print(f"the subtraction of the two number is ::" ,{subtraction})
+# print(f"the mul of the two number is::  ",{mul})
+print(f"the divide of the two number is::  ",{divide})
